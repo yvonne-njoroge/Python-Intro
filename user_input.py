0272723541d10e92d41610ae7a30bb6118c08159
Yvonne Njoroge <ynjoroge@chlorideexide.com>
2024-11-05 12:10:16 +0300

    personalized greetings

---
+++ user_input.py
@@ -0,0 +1,7 @@
+name = input('What is your name')
+
+age = input('how old are you?')
+
+location = input('where are you from?')
+
+print(f"Hello, {name}! It's great to meet someone who is {age} years old from {location}. Hope you're having a fantastic day!")
